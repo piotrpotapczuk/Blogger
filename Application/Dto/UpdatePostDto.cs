d88aa4674d8e51c0a0be896cefd09151e5eff73a
piotr.potapczuk <piotr.potapczuk@gmail.com>
2022-09-16 12:57:16 +0200

    END - Konfiguracja AutoMappera

--- Application/Dto/UpdatePostDto.cs
+++ Application/Dto/UpdatePostDto.cs
@@ -1,8 +1,17 @@
-﻿namespace Application.Dto
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Dto
 {
-    public class UpdatePostDto
+    public class UpdatePostDto : IMap
     {
         public int Id { get; set; }
         public string Content { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdatePostDto, Post>();
+        }
     }
 }
